3be88915b3e3c7da3472a5db8515d294e5ca8e1d
Creeper011 <75911169+Creeper011@users.noreply.github.com>
2026-02-27 22:52:01 -0300

    feat: add an better cli

--- yumly_cli.nim
+++ yumly_cli.nim
@@ -1,16 +1,57 @@
-
-
 import os
+import std/terminal
 import src/Yumly/libyumly
+
+template info(msg: string) =
+    setForegroundColor(fgYellow)
+    echo msg
+    resetAttributes()
+
+template success(msg: string) =
+    setForegroundColor(fgGreen)
+    echo msg
+    resetAttributes()
+
+template error(msg: string) =
+    setForegroundColor(fgRed)
+    echo msg
+    resetAttributes()
+
+proc checkCommand(content: string): bool =
+    if fileExists(content):
+        info("Validating file...")
+        result = validateFile(content)
+    else:
+        info("Validating content...")
+        result = validateContent(content)
+
+proc loadCommand(path: string): auto =
+  loadYumly(path)
 
 proc parseOutput() =
     let args = commandLineParams()
-    if args.len > 0:
-        try:
-            echo loadYumly(args[0])
-        except CatchableError as e:
-            echo e.msg
-    else:
-        echo "please provide a .yumly file path as an argument"
-  
+
+    if args.len < 2:
+        error("usage: yumly <check|load> <file|content>")
+        return
+
+    let cmd = args[0]
+    let value = args[1]
+
+    try:
+        case cmd
+        of "check":
+            if checkCommand(value):
+                success("Check passed ✔")
+            else:
+                error("Check failed ✖")
+        of "load":
+            let data = loadCommand(value)
+            echo data
+            success("File loaded ✔")
+        else:
+            error("Unknown command: " & cmd)
+    except CatchableError as error:
+        error(error.msg)
+
 parseOutput()
