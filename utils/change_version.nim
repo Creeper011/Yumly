70c97809d37d50607733f96abfa5292afaedc545
Creeper011 <75911169+Creeper011@users.noreply.github.com>
2026-03-30 13:24:31 -0300

    feat: support optional git tagging in version update utility; bump project version to 0.8.2 aligned with git tag [skip ci]

--- utils/change_version.nim
+++ utils/change_version.nim
@@ -1,4 +1,4 @@
-import os, strutils
+import os, strutils, osproc
 
 const
     files = @[
@@ -6,17 +6,25 @@ const
         "pyproject.toml",
     ]   
 
-proc parseOutputVersion(): string =
+proc parseOutputVersion(): (string, bool) =
     let args = commandLineParams()
     if args.len < 1:
-        echo "Usage: change_version <new_version>"
+        echo """
+        Usage: change_version <new_version> <-tag>
+        
+        <new_version> : the new version to set
+        <-tag>      : optional, if true, will add a git tag to the new version with prefix 'v'
+        """
         quit()
-    args[0]
+    (args[0], args.len > 1 and args[1] == "-tag")
 
-proc changeVersion(version: string) =
+proc changeVersion(version: string, addTag: bool) =
     let root = currentSourcePath().parentDir() / ".."
     for file in files:
         let path = root / file
+        if not path.fileExists():
+            echo "Error: File not found: " & path
+            quit(1)
         let content = readFile(path)
         var lines = content.splitLines()
         for i, line in lines:
@@ -26,5 +34,11 @@ proc changeVersion(version: string) =
                 if quoteStart != -1 and quoteEnd != -1 and quoteStart != quoteEnd:
                     lines[i] = line[0..quoteStart] & version & line[quoteEnd..^1]
         writeFile(path, lines.join("\n"))
+    if addTag:
+        let result = execCmd("git tag v" & version)
+        if result != 0:
+            echo "Error: Failed to add git tag"
+            quit(1)
 
-parseOutputVersion().changeVersion()
+let (version, addTag) = parseOutputVersion()
+changeVersion version, addTag
